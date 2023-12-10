9f8f66aafad0b20668fcc7b1b6dca3a63961860b
xyb12345678qwe <xyb12345678qwe@hotmail.com>
2023-12-10 17:39:53 +0800

    更新

--- 更新.bat
+++ 更新.bat
@@ -1,4 +1,15 @@
+@echo off
+setlocal
+
+echo Committing changes...
+set /P COMMIT_MSG=请输入提交信息：
+chcp 936 > nul
 git init
 git add .
-git commit -m "更新大量功能"
-git push -u origin master
+git commit -m "%COMMIT_MSG%"
+
+echo Pushing changes to Gitee...
+git push origin %BRANCH_NAME%
+
+echo Done.
+endlocal
