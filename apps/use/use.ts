7e57529d3494608f29413eacd9da6001e9c526d1
xyb12345678qwe <xyb12345678qwe@hotmail.com>
2023-12-31 21:42:22 +0800

    更新

--- apps/use/use.ts
+++ apps/use/use.ts
@@ -33,13 +33,14 @@ export class use extends APlugin  {
             if(!thing) return e.reply(`没有找到这个物品`)
             let { player, bag } = results;
             let bag_thing = bag[thing.type].find(item => item.name === thing_name)
+            let shezhi = await Read_json(2);
+            const daoju_shezhi = shezhi.find(item => item.type == "道具类");
             if (!thing.能否消耗 && thing.能否消耗 == "否") return e.reply(`此物品不能使用`);
             if (!bag_thing) return e.reply(`你没有这个物品`);
             if (bag_thing.数量 < num) return e.reply(`数量不足,还差${Number(num) - bag_thing.数量}`);
             let msg = [`使用成功`];
             if(thing.type == "功法"){
                 const gongfa = bag.已学习功法.find(item => item.name == thing_name);
-
                 if(gongfa) return e.reply('已学习过'+ thing_name)
                 else{
                     thing.all_功效.forEach((element:number) => {
@@ -48,12 +49,15 @@ export class use extends APlugin  {
                 }
             }
             if (thing_name.includes(`灵器经验石`)) {
-                let shezhi = await Read_json(2);
-                const list = shezhi.find(item => item.type == "道具类");
-                const exp = list.thing[thing_name] * Number(num)
+                const exp = daoju_shezhi.thing[thing_name] * Number(num)
                 if (!exp) return e.reply(`数值暂未设置`)
                 player.本命灵器.exp += exp;
                 msg.push(`灵器经验增加${exp},目前经验${player.本命灵器.exp}`);
+            }else if(thing_name.includes(`聚神丹`)){
+                const exp = daoju_shezhi.thing[thing_name] * Number(num)
+                if (!exp) return e.reply(`数值暂未设置`)
+                player.灵魂力量 += exp;
+                msg.push(`灵魂力量增加${exp},目前灵魂力量${player.灵魂力量}`);
             }
             if (msg.length === 1 && thing.type !== "功法") return e.reply(`道具目前没有设置可使用的处理代码`);
             if(thing.type == "功法")thing.type ="已学习功法";
