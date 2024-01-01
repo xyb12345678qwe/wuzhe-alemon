43318b8ddf4c9af4e3de1d7d595e03740e55b9db
xyb12345678qwe <xyb12345678qwe@hotmail.com>
2024-01-01 10:04:41 +0800

    更新

--- db/item/灵器列表.ts
+++ db/item/灵器列表.ts
@@ -22,9 +22,18 @@ export const 灵器列表 = sequelize.define("灵器列表", {
 });
 // const tableAttributes = await 灵器列表.findAll({raw:true});
 // console.log(tableAttributes);
-await 灵器列表.sync() 
+// await 灵器列表.sync() 
 await 灵器列表.sync({ force: true });
 let list = await Read_json(4,'/灵器列表.json');
 list.forEach(item => {
-    灵器列表.create({name:item.name,品级:item.品级,攻击加成:item.攻击加成,防御加成:item.防御加成,生命加成:item.生命加成,暴击加成:item.暴击加成,爆伤加成:item.爆伤加成,闪避加成:item.闪避加成})
+    灵器列表.create({
+        name:item.name,
+        品级:item.品级,
+        攻击加成:item.攻击加成,
+        防御加成:item.防御加成,
+        生命加成:item.生命加成,
+        暴击加成:item.暴击加成,
+        爆伤加成:item.爆伤加成,
+        闪避加成:item.闪避加成,
+    })
 })
